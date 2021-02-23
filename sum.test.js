eb8b5ca724ab19968971b6d5b97124149f642c89
MarjanSirovljevic <marjan.sirovljevic@tx.group>
2021-02-23 20:30:09 +0100

    tests updated

--- sum.test.js
+++ sum.test.js
@@ -3,3 +3,7 @@ const sum = require('./sum');
 test('1 plus 2 equals to 3', function() {
     expect(sum(1, 2)).toBe(3);
 });
+
+test('1 plus 3 equals to 5', function() {
+    expect(sum(1, 3)).toBe(5);
+});
